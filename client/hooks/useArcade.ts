1f5c1062498a3eb92e203c073f753fc77d931318
Nixon <214925225+Ncam00@users.noreply.github.com>
2025-09-11 11:56:14 +1200

    navbar working probably

--- client/hooks/useArcade.ts
+++ client/hooks/useArcade.ts
@@ -1,13 +1,13 @@
 import { useQuery } from '@tanstack/react-query'
 import request from 'superagent'
 
-import type { Arcade } from '../Arcade.ts'
+import type { Arcade } from '../../models/Arcade.ts'
 
 const rootURL = new URL(`/api/v1`, document.baseURI)
 
 export default function useArcade() {
   return useQuery({
-    queryKey: ['arcade'],
+    queryKey: ['arcades'],
     queryFn: async () => {
       const res = await request.get(`${rootURL}/arcades`)
       if (res.ok) {
---
+++ models/Arcade.ts
@@ -0,0 +1,8 @@
+export interface ArcadeData {
+  name: string
+  description: string
+}
+
+export interface Arcade extends ArcadeData {
+  id: number
+}
