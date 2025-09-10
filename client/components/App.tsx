a9e049d045c5119dd7d55577ebfb98c5eb803758
aeron-glasgow-keene <a.glasgowkeene@gmail.com>
2025-09-10 16:44:02 +1200

    feat: add sprite image and movement, some extra code to prepare for backend linkup

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,7 +1,12 @@
 import { usePokemon } from '../hooks/usePokemon.ts'
+import PlayerSprite from './PlayerSprite.tsx'
+import { useEffect, useState } from 'react'
+
 
 function App() {
   const { data } = usePokemon()
+
+    const [position, setPosition] = useState({ x: 80, y: 140 })
 
   return (
     <>
@@ -12,6 +17,7 @@ function App() {
         <ul>
           {data && data.map((pokemon) => <li key={pokemon}>{pokemon}</li>)}
         </ul>
+        <PlayerSprite position={position} setPosition={setPosition} />
       </div>
     </>
   )
