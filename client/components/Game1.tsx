89f7169147b4da1173e3ba1e68d3b69a44b7300e
Annie <215331755+anniemoffatt@users.noreply.github.com>
2025-09-12 14:29:09 +1200

    (feat): pokemon generator, work in progress

--- client/components/Game1.tsx
+++ client/components/Game1.tsx
@@ -1,5 +1,6 @@
 import PlayerSprite from './PlayerSprite.tsx'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import Mons from './WildMonsGenerator.tsx'
 
 // boundaries suss
 // use abe's code (but without images) to specify points on screen (within boundaires) that trigger a console.log()
@@ -11,9 +12,50 @@ import { useRef, useState } from 'react'
 // can catch
 // stretch - can battle
 
+
+
+// Abe's mons code:
+// const initalmons = [
+//   { id: 1, top: 660, left: 1000, collected: false },
+// ]
+
+const initalMons = [
+  { id: 1, top: 660, left: 1000 },
+  { id: 2, top: 560, left: 500 },
+  { id: 3, top: 340, left: 1100 },
+  { id: 4, top: 740, left: 1300 },
+  { id: 5, top: 140, left: 700 },
+  { id: 6, top: 200, left: 1900 },
+  { id: 7, top: 800, left: 600 },
+  { id: 8, top: 300, left: 420 },
+  { id: 9, top: 600, left: 1800 },
+]
+
+
 function Game1() {
   const [position, setPosition] = useState({ x: 80, y: 140 })
+  const [mons, setMons] = useState({ initalMons })
+
   const containerRef = useRef<HTMLDivElement>(null)
+
+
+   useEffect(() => {
+    {
+      mons.map((mon, i) => {
+        if (
+          position.x + 50 > mons[i].left &&
+          position.x + 50 <= mons[i].left + 40 &&
+          position.y + 50 > mons[i].top &&
+          position.y + 50 <= mons[i].top + 40
+        ) {
+          const current = [...mons]
+          current[i].collected = true
+          setMons(current)
+        }
+      })
+    }
+  }, [position])
+
 
   return (
     <div>
@@ -25,6 +67,10 @@ function Game1() {
           setPosition={setPosition}
           containerRef={containerRef}
         />
+        < Mons mons={mons} setMons={setMons}
+          />
+          {/* containerRef={containerRef} */}
+        
       </div>
     </div>
   )
--- client/components/PlayerSprite.tsx
+++ client/components/PlayerSprite.tsx
@@ -5,6 +5,13 @@ import spriteUp from '/images/player-sprite-standing-facing-away.png'
 import spriteLeft from '/images/player-sprite-standing-facing-left.png'
 import spriteRight from '/images/player-sprite-standing-facing-right.png'
 
+// SPRINT TO DOS:
+// Add the pngs for walking and add another state for isWalking based on key down / key up
+// Resize all pngs to be exactly the same width with the sprite exactly centre (so that the sprite doesn't slowly diagnally move when spamming up left up left etc)
+// Add the equivalent images for female player and change sprite images based on backend "user" data
+
+// Match directions to the corresponding sprite image
+
 const sprites = {
   up: spriteUp,
   down: spriteDown,
@@ -12,21 +19,19 @@ const sprites = {
   right: spriteRight,
 }
 
-// SPRINT TO DOS:
-// Add the pngs for walking and add another state for isWalking based on key down / key up
-// Resize all pngs to be exactly the same width with the sprite exactly centre (so that the sprite doesn't slowly diagnally move when spamming up left up left etc)
-// Add the equivalent images for female player and change sprite images based on backend "user" data
+// Sprite and step size dimensions
 
-// Define step and sprite sizes:
 const spriteWidthNum: number = 22.5 // for clamping math
 const spriteWidthCss: string = `${spriteWidthNum}px` // for CSS
 const step: number = 20
 
+// Sprite coordinate types
 interface Sprite {
   x: number
   y: number
 }
 
+// Props passed from Game1.tsx
 interface Props {
   position: Sprite
   setPosition: React.Dispatch<React.SetStateAction<Sprite>>
@@ -37,24 +42,38 @@ interface Props {
 // import { User } from '../../models/models'
 // import { usePlayerSprite } from '../hooks/usePlayerSprite'
 
+// Main sprite component 
+
 function PlayerSprite({ position, setPosition, containerRef }: Props) {
     // Use this next line when the player sprite changes appearance based on gender selection in the "user" table in the database
   // const player = usePlayerSprite()
 
   const [direction, setDirection] = useState<'up' | 'down' | 'left' | 'right'>('down')
 
+  // Handle keyboard input and sprite movement
+
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
       if (['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(e.key)) {
         e.preventDefault()
       }
+      
+      // Get map dimensions for boundary 
 
       const container = containerRef.current
+ 
+      // If container hasn't loaded, return nothing (to prevent errors) Annie and Aeron to check with facilitator at some point
+
       if (!container) return
 
-      const rect = container.getBoundingClientRect()
+      // Fun fact rect is short for rectangle not rectum
+
+      const rect = container.getBoundingClientRect() // every rectangle is a love rectangle when you love rectangles
       const maxX = rect.width - spriteWidthNum
       const maxY = rect.height - spriteWidthNum
+
+      // Change sprite position based on keyboard input
+      // Adjust position and update direction sprite is facing
 
       setPosition((prev) => {
         let newX = prev.x
@@ -79,6 +98,8 @@ function PlayerSprite({ position, setPosition, containerRef }: Props) {
             break
         }
 
+        // BOUNDARY SETTING
+
         return {
           x: Math.min(Math.max(newX, 0), maxX),
           y: Math.min(Math.max(newY, 0), maxY),
@@ -86,10 +107,14 @@ function PlayerSprite({ position, setPosition, containerRef }: Props) {
       })
     }
 
+    // Listening for key presses
+
     window.addEventListener('keydown', handleKeyDown)
     return () => window.removeEventListener('keydown', handleKeyDown)
   }, [containerRef, setPosition])
 
+  // Renders sprite at current position 
+  
   return (
     <div
       style={{
