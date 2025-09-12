f0759fe692e8e2e3e3775e57bb5f50c07a119474
Nixon <214925225+Ncam00@users.noreply.github.com>
2025-09-12 12:13:40 +1200

    navbar css good

--- client/components/NavPokemon.tsx
+++ client/components/NavPokemon.tsx
@@ -3,10 +3,25 @@ import { NavLink } from 'react-router'
 export default function Navbar() {
   return (
     <nav>
-      <NavLink to="/home">Home</NavLink>
-      <NavLink to="/game-1/"> Game 1 </NavLink>
-      <NavLink to="/game-2/"> Who&apos;s that Pokemon! </NavLink>
-      <NavLink to="/game-3/"> Tic-Tac-Johto </NavLink>
+      <NavLink className="navButtons" to="/home">
+        Home
+      </NavLink>
+
+      <NavLink className="navButtons" to="/game-1/">
+        {' '}
+        Game 1{' '}
+      </NavLink>
+
+      <NavLink className="navButtons" to="/game-2/">
+        {' '}
+        Who&apos;s that Pokemon!{' '}
+      </NavLink>
+
+      <NavLink className="navButtons" to="/game-3/">
+        {' '}
+        Tic-Tac-Johto{' '}
+      </NavLink>
+
       {/* <NavLink className="nav" to="/arcade">
         view games
       </NavLink> */}
