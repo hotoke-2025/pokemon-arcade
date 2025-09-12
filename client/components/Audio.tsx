123d11087dd9f801844cbdf2296d01f973f04f90
Kaylin Chu <kaylinltj@hotmail.com>
2025-09-12 14:56:38 +1200

    Feat: added in audio and controls colour same as background

--- client/components/Audio.tsx
+++ client/components/Audio.tsx
@@ -1,13 +1,20 @@
 import pokeAudio from '../../Pokémon Theme Song.mp3'
 const pokesound = pokeAudio
-function MyAudioPlayer() {
-      return (
-        <div className='mx-auto'>
-          <audio controls src={pokesound}>
-        </audio>
-        </div>
+function Audio() {
+  return (
+    <div className="mx-auto">
+      <audio autoPlay muted loop controls src={pokesound}>
+        <track
+          kind="captions"
+          src="captions.vtt"
+          srcLang="en"
+          label="English"
+          default
+        />
+        Your browser does not support the audio element.
+      </audio>
+    </div>
+  )
+}
 
-      );
-    };
-
-    export default MyAudioPlayer;
+export default Audio
