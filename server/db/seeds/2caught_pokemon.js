06d69dbca59fafbd40bb30bb7067fcd6197efddc
Kaylin Chu <kaylinltj@hotmail.com>
2025-09-17 09:48:23 +1200

    Feat: Images hardcoded into seeds

--- server/db/seeds/2caught_pokemon.js
+++ server/db/seeds/2caught_pokemon.js
@@ -11,10 +11,44 @@ export async function seed(knex) {
       nickname: 'G',
       released: false,
       user_id: 1,
+      image:
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/58.png',
     },
-    { id: '2', name: 'Mew', nickname: 'M', released: false, user_id: 2 },
-    { id: '3', name: 'Squirtle', nickname: 'S', released: false, user_id: 3 },
-    { id: '4', name: 'Pikachu', nickname: 'P', released: false, user_id: 4 },
-    { id: '5', name: 'Tepig', nickname: 'T', released: false, user_id: 5 },
+    {
+      id: '2',
+      name: 'Mew',
+      nickname: 'M',
+      released: false,
+      user_id: 2,
+      image:
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/151.png',
+    },
+    {
+      id: '3',
+      name: 'Squirtle',
+      nickname: 'S',
+      released: false,
+      user_id: 3,
+      image:
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png',
+    },
+    {
+      id: '4',
+      name: 'Pikachu',
+      nickname: 'P',
+      released: false,
+      user_id: 4,
+      image:
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png',
+    },
+    {
+      id: '5',
+      name: 'Tepig',
+      nickname: 'T',
+      released: false,
+      user_id: 5,
+      image:
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/498.png',
+    },
   ])
 }
